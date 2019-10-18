7a9d2c51e59f0c2cc15c274b2c05fbfb4995ea40
iSarveshK <sarveshkuthe7@gmail.com>
2019-10-18 14:26:28 -0700

    Updated Login

--- Login&SignupPages/login.js
+++ Login&SignupPages/login.js
@@ -1,23 +1,19 @@
-function validateloginform(){
-	var email =   document.getElementById("EMail").value;
-    if (email == "")                                   
-    { 
-        document.getElementById("alert2").innerHTML ="Please enter a valid e-mail address.";
-    } 
-   
-    if (email.indexOf("@", 0) < 0)                 
-    { 
-        document.getElementById("alert2").innerHTML ="Please enter a valid e-mail address.";
-    } 
-   
-    if (email.indexOf(".", 0) < 0)                 
-    { 
-        document.getElementById("alert2").innerHTML ="Please enter a valid e-mail address.";  
-    }
+function validateloginform() {
+	var email = document.getElementById("EMail").value;
+	if (email == "") {
+		document.getElementById("alert2").innerHTML = "!Please enter a valid e-mail address.";
+	}
 
-    var password =  document.getElementById("Password").value; 
-    if (password == "")                        
-    {   
-        document.getElementById("alert3").innerHTML ="Please enter your password.";
-    } 
+	if (email.indexOf("@", 0) < 0) {
+		document.getElementById("alert2").innerHTML = "!Please enter a valid e-mail address.";
+	}
+
+	if (email.indexOf(".", 0) < 0) {
+		document.getElementById("alert2").innerHTML = "!Please enter a valid e-mail address.";
+	}
+
+	var password = document.getElementById("Password").value;
+	if (password == "") {
+		document.getElementById("alert3").innerHTML = "!Please enter your password.";
+	}
 }
--- Login&SignupPages/signup.js
+++ Login&SignupPages/signup.js
@@ -1,52 +1,41 @@
+function validateForm() {
 
-  function validateForm() {
-    
-    var name = document.getElementById("name").value;
-    if (name == "") 
-    {
-      document.getElementById("alert").innerHTML ="Name must be filled out";
-      
-    }
+	var name = document.getElementById("name").value;
+	if (name == "") {
+		document.getElementById("alert").innerHTML = "!Name must be filled out";
+	}
 
-    var phone =  document.getElementById("Telephone").value;
-     if (phone == "" )                           
-    {   
-     
-        document.getElementById("alert1").innerHTML ="Please enter your telephone number.";  
-    }
-    var phoneno = /^\d{10}$/;
-     if(!phone.match(phoneno))
-     {
-        document.getElementById("alert1").innerHTML ="Please enter your valid telephone number.";
-    }
+	var phone = document.getElementById("Telephone").value;
+	if (phone == "") {
 
-    var email =   document.getElementById("EMail").value;
-    if (email == "")                                   
-    { 
-        document.getElementById("alert2").innerHTML ="Please enter a valid e-mail address.";
-    } 
-   
-    if (email.indexOf("@", 0) < 0)                 
-    { 
-        document.getElementById("alert2").innerHTML ="Please enter a valid e-mail address.";
-    } 
-   
-    if (email.indexOf(".", 0) < 0)                 
-    { 
-        document.getElementById("alert2").innerHTML ="Please enter a valid e-mail address.";  
-    }
+		document.getElementById("alert1").innerHTML = "!Please enter your telephone number.";
+	}
+	var phoneno = /^\d{10}$/;
+	if (!phone.match(phoneno)) {
+		document.getElementById("alert1").innerHTML = "!Please enter your valid telephone number.";
+	}
 
-    var password =  document.getElementById("Password").value; 
-    if (password == "")                        
-    {   
-        document.getElementById("alert3").innerHTML ="Please enter your password.";
-    } 
+	var email = document.getElementById("EMail").value;
+	if (email == "") {
+		document.getElementById("alert2").innerHTML = "!Please enter a valid e-mail address.";
+	}
 
-    var pass = /^\d{6}$/;
-     if(!password.match(pass))
-     {
-        document.getElementById("alert3").innerHTML ="Please enter your password.";
-    } 
-  
-   
+	if (email.indexOf("@", 0) < 0) {
+		document.getElementById("alert2").innerHTML = "!Please enter a valid e-mail address.";
+	}
+
+	if (email.indexOf(".", 0) < 0) {
+		document.getElementById("alert2").innerHTML = "!Please enter a valid e-mail address.";
+	}
+
+	var password = document.getElementById("Password").value;
+	if (password == "") {
+		document.getElementById("alert3").innerHTML = "!Please enter your password.";
+	}
+
+	var pass = /^\d{6}$/;
+	if (!password.match(pass)) {
+		document.getElementById("alert3").innerHTML = "!Please enter valid password.";
+	}
+
 }
